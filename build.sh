c18c25434925a8fa3da5b7c81363518b1d7a9476
Gustavo Salibi <gustavo.salibi@gmail.com>
2019-08-27 13:19:45 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -14,27 +14,48 @@ status=$(curl -s --head -w %{http_code} https://oplab9.parqtec.unicamp.br/pub/pp
 
 if [  $status == 404 ] 
 then
+    echo "VALIDACAO 1"
     sudo apt install -y lftp
+    
+    echo "VALIDACAO 2"
     status=$(curl -s --head -w %{http_code} https://oplab9.parqtec.unicamp.br/pub/ppc64el/docker/version-$github_version -o /dev/null)
+    
+    echo "VALIDACAO 3"
     if [[ $status == 404 ]]
     then
+        echo "VALIDACAO 3.5"
         lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; mkdir /ppc64el/docker/version-$github_version"
         #lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /ppc64el/docker/$ftp_version"
     fi
     
+    echo "VALIDACAO 4"
     wget https://github.com/docker/docker-ce/archive/v$github_version.zip
+    
+    echo "VALIDACAO 5"
     unzip v$github_version.zip
+    
+    echo "VALIDACAO 6"
     mv docker-ce-$github_version docker-ce
-#    cd docker-ce && git apply --3way ../patches/*
+    
+    echo "VALIDACAO 7"
+    cd docker-ce && git apply --3way ../patches/*
+    
+    echo "VALIDACAO 8"
     cd docker-ce/$dir
+    
+    echo "VALIDACAO 9"
     VERSION=$github_version make $sys
+    
+    echo "VALIDACAO 10"
     cd ../../../
+    
+    echo "VALIDACAO 11"
     cd $bin_dir
     
     #if [[ $github_version > $ftp_version ]]
     #then
         
     #fi
-    
+    echo "VALIDACAO 12"
     lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; mirror -R ./ /ppc64el/docker/version-$github_version/$sys"
 fi
