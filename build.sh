270675d80c67aea8bf00d500bb902bba20e7cab3
Vinicius Couto Espindola <vinicius.c.e@hotmail.com>
2019-11-04 19:38:13 -0300

    Installing docker at runtime

--- build.sh
+++ build.sh
@@ -1,61 +1,37 @@
-#github_version=$(cat github_version.txt)
-#github_version="18.09.0"
-#github_version="18.09.1"
-#github_version="18.09.2"
-#github_version="18.09.3"
-#github_version="18.09.4"
-#github_version="18.09.5"
-#github_version="18.09.6"
-#github_version="18.09.7"
-github_version="19.03.1"
-ftp_version=$(cat ftp_version.txt)
-del_version=$(cat delete_version.txt)
-status=$(curl -s --head -w %{http_code} https://oplab9.parqtec.unicamp.br/pub/ppc64el/docker/version-$github_version/$sys -o /dev/null)
+ftp_path="ftp://oplab9.parqtec.unicamp.br/test/vinicius/docker"
+url="https://oplab9.parqtec.unicamp.br/pub/test/vinicius/docker"
 
-if [  $status == 404 ] 
+home_dir=$(pwd)
+git_ver=$(cat github_version.txt)
+ftp_ver=$(cat ftp_version.txt)
+# del_version=$(cat delete_version.txt)
+
+echo "=========> [CHECKING IF BUILD EXISTS] >>> "
+status=$(curl -s --head -w %{http_code} $url/version-$git_ver/$sys -o /dev/null) 
+if [ $status == 404 ] 
 then
-    echo "VALIDACAO 1"
+
+    echo "=========> [CREATING FTP FOLDER] >>> "
     sudo apt install -y lftp
-    
-    echo "VALIDACAO 2"
-    status=$(curl -s --head -w %{http_code} https://oplab9.parqtec.unicamp.br/pub/ppc64el/docker/version-$github_version -o /dev/null)
-    
-    echo "VALIDACAO 3"
-    if [[ $status == 404 ]]
-    then
-        echo "VALIDACAO 3.5"
-        lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; mkdir /ppc64el/docker/version-$github_version"
-        #lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /ppc64el/docker/$ftp_version"
-    fi
-    
-    echo "VALIDACAO 4"
-    wget https://github.com/docker/docker-ce/archive/v$github_version.zip
-    
-    echo "VALIDACAO 5"
-    unzip v$github_version.zip
-    
-    echo "VALIDACAO 6"
-    mv docker-ce-$github_version docker-ce
-    
-    echo "VALIDACAO 7"
-    cd docker-ce && git apply --3way ../patches/*
-    
-    echo "VALIDACAO 8"
-    cd $dir
-    
-    echo "VALIDACAO 9"
-    VERSION=$github_version make $sys
-    
-    echo "VALIDACAO 10"
-    cd ../../../
-    
-    echo "VALIDACAO 11"
-    cd $bin_dir
-    
-    #if [[ $github_version > $ftp_version ]]
-    #then
-        
-    #fi
-    echo "VALIDACAO 12"
-    lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; mirror -R ./ /ppc64el/docker/version-$github_version/$sys"
+    lftp -c "open -u $USER,$PASS $ftp_path; mkdir -p version-$git_ver/$sys"
+
+    echo "=========> [INSTALLING DOCKER] >>> "
+    git clone https://github.com/Unicamp-OpenPower/docker.git
+    sudo snap install docker
+
+    echo "=========> [CLONNING <$git_ver> AND PATCHING] >>>"
+    git clone https://github.com/docker/docker-ce
+    cd docker-ce && git checkout v$git_ver
+    git apply -v --3way ../patches/*
+
+    echo "=========> [BUILDING <$sys> PACKAGES] >>>"
+    cd $home_dir/$dir
+    VERSION=$git_ver make $sys
+
+    echo "=========> [SENDING PACKAGES TO FTP] >>>"
+    cd $home_dir/$bin_dir
+    lftp -c "open -u $USER,$PASS $ftp_path/version-$git_ver/$sys; mirror -R ./ ./"
+
 fi
+
+echo "=========> [DONE]"
--- ftp_version.py
+++ ftp_version.py
@@ -1,36 +1,28 @@
 import requests
-# find and save the current Github release
-html = str(
-           requests.get('https://github.com/docker/docker-ce/releases/latest')
-           .content)
-index = html.find('Release ')
-github_version = html[index + 8:index + 15]
-file = open('github_version.txt', 'w')
-file.writelines(github_version)
-file.close()
+from re import findall
 
+# function get_info
+#   Creates a file <name> whith the highest version found of a version 
+#   (string) within the given path. If not found, writes '0'  
+# Parameters:
+#   path  - URL where to look for the info
+#   regex - Pattern of what to look for in the path to locate the info
+#   name  - Name of the text file to be written with the info
+#   cut -  funtion indicating what to keep from the regex pattern
+def get_info(path, regex, name, cut=(lambda x : x)):
+    html = str(requests.get(path).content)
+    info = cut(max(findall(regex, html)+["0"]))
+    file = open(name+'.txt', 'w')
+    file.writelines(info)
+    file.close()
+
+# Define the FTP URL for downloading and uploading packages
+# ftp_path = 'https://oplab9.parqtec.unicamp.br/pub/ppc64el/docker'
+ftp_path = 'https://oplab9.parqtec.unicamp.br/pub/test/vinicius/'
+git_path = 'https://github.com/docker/docker-ce/releases/latest'
+
+# find and save the current Github release
+get_info(git_path, 'v\d\d[.]\d\d[.]\d', 'github_version', cut=(lambda x : x[1:]))
 
 # find and save the current Docker version on FTP server
-html = str(
-             requests.get(
-                        'https://oplab9.parqtec.unicamp.br/pub/ppc64el/docker'
-                        ).content)
-index = html.rfind('version-')
-ftp_version = html[index + 8:index + 15]
-file = open('ftp_version.txt', 'w')
-file.writelines(ftp_version)
-file.close()
-
-# find and save the oldest Bazel version on FTP server
-index = html.find('version-')
-delete = html[index + 8:index + 15]
-file = open('delete_version.txt', 'w')
-file.writelines(delete)
-file.close()
-
-# find and save the oldest Bazel version on FTP server
-index = html.find('ddddw')
-file = open('create_dir.txt', 'w')
-file.writelines(str(index))
-file.close()
-
+get_info(ftp_path, '\d\d[.]\d\d[.]\d', 'ftp_version')
