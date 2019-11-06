ad4a061607e4b702b02b28d09d1eef5db405ab80
vinicius espindola <vinicius.c.e@hotmail.com>
2019-11-05 21:21:28 -0300

    Repeating timed out builds

--- build.sh
+++ build.sh
@@ -6,32 +6,26 @@ home_dir=$(pwd)
 ftp_ver=$(cat ftp_version.txt)
 # del_version=$(cat delete_version.txt)
 
-echo "=========> [CHECKING IF BUILD EXISTS] >>> "
-status=$(curl -s --head -w %{http_code} $url/version-$git_ver/$sys -o /dev/null) 
-if [ $status == 404 ] 
-then
+echo "=========> [CREATING FTP FOLDER] >>> "
+sudo apt install -y lftp
+lftp -c "open -u $USER,$PASS $ftp_path; mkdir -p version-$git_ver/$sys"
 
-    echo "=========> [CREATING FTP FOLDER] >>> "
-    sudo apt install -y lftp
-    lftp -c "open -u $USER,$PASS $ftp_path; mkdir -p version-$git_ver/$sys"
+echo "=========> [INSTALLING DOCKER] >>> "
+git clone https://github.com/Unicamp-OpenPower/docker.git
+sudo snap install docker
 
-    echo "=========> [INSTALLING DOCKER] >>> "
-    git clone https://github.com/Unicamp-OpenPower/docker.git
-    sudo snap install docker
+echo "=========> [CLONNING <$git_ver> AND PATCHING] >>>"
+git clone https://github.com/docker/docker-ce
+cd docker-ce && git checkout v$git_ver
+git apply -v --3way ../patches/*
 
-    echo "=========> [CLONNING <$git_ver> AND PATCHING] >>>"
-    git clone https://github.com/docker/docker-ce
-    cd docker-ce && git checkout v$git_ver
-    git apply -v --3way ../patches/*
+echo "=========> [BUILDING <$sys> PACKAGES] >>>"
+cd $home_dir/$dir
+VERSION=$git_ver make $sys
 
-    echo "=========> [BUILDING <$sys> PACKAGES] >>>"
-    cd $home_dir/$dir
-    VERSION=$git_ver make $sys
+echo "=========> [SENDING PACKAGES TO FTP] >>>"
+cd $home_dir/$bin_dir
+lftp -c "open -u $USER,$PASS $ftp_path/version-$git_ver/$sys; mirror -R ./ ./"
 
-    echo "=========> [SENDING PACKAGES TO FTP] >>>"
-    cd $home_dir/$bin_dir
-    lftp -c "open -u $USER,$PASS $ftp_path/version-$git_ver/$sys; mirror -R ./ ./"
-
-fi
 
 echo "=========> [DONE]"
