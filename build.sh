3b83949b10863146bff38522c8222a2c50401687
Vinicius Couto Espindola <vini.couto.e@gmail.com>
2020-07-01 10:21:46 -0300

    Updated missing releases script

--- build.sh
+++ build.sh
@@ -3,32 +3,23 @@ ftp_path="ftp://oplab9.parqtec.unicamp.br/ppc64el/docker"
 url="https://oplab9.parqtec.unicamp.br/pub/ppc64el/docker"
 
 home_dir=$(pwd)
-git_ver="19.03.10"
-ftp_ver=$(cat ftp_version.txt)
-# del_version=$(cat delete_version.txt)
+git_ver="19.03.11"
 
-echo "=========> [CHECKING IF BUILD EXISTS] >>> "
-status=$(curl -s --head -w %{http_code} $url/version-$git_ver/$sys -o /dev/null) 
-if [ $status == 404 ] 
-then
+echo "=========> [CLONNING <$git_ver> AND PATCHING] >>>"
+git clone https://github.com/docker/docker-ce
+cd docker-ce && git checkout v$git_ver
+git am --3way ../patches/*
 
-    echo "=========> [CLONNING <$git_ver> AND PATCHING] >>>"
-    git clone https://github.com/docker/docker-ce
-    cd docker-ce && git checkout v$git_ver
-    git am --3way ../patches/*
+echo "=========> [BUILDING <$sys> PACKAGES] >>>"
+cd $home_dir/$dir
+sudo VERSION=$git_ver make $sys
 
-    echo "=========> [BUILDING <$sys> PACKAGES] >>>"
-    cd $home_dir/$dir
-    sudo VERSION=$git_ver make $sys
+echo "=========> [CREATING FTP FOLDER] >>> "
+lftp -c "open -u $USER,$PASS $ftp_path; mkdir -p version-$git_ver/$sys"
 
-    echo "=========> [CREATING FTP FOLDER] >>> "
-    lftp -c "open -u $USER,$PASS $ftp_path; mkdir -p version-$git_ver/$sys"
-
-    echo "=========> [SENDING PACKAGES TO FTP] >>>"
-    cd $home_dir/$bin_dir
-    lftp -c "open -u $USER,$PASS $ftp_path/version-$git_ver/$sys; mirror -R ./ ./"
-    sudo rm -rf $home_dir/$bin_dir
-
-fi
+echo "=========> [SENDING PACKAGES TO FTP] >>>"
+cd $home_dir/$bin_dir
+lftp -c "open -u $USER,$PASS $ftp_path/version-$git_ver/$sys; mirror -R ./ ./"
+sudo rm -rf $home_dir/$bin_dir
 
 echo "=========> [DONE]"
