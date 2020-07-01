715769e0a368b5f262ee2f563b136bd751d0b598
Vinicius Couto Espindola <vini.couto.e@gmail.com>
2020-07-01 10:17:32 -0300

    Updated regex and latest version search

--- ftp_version.py
+++ ftp_version.py
@@ -1,6 +1,9 @@
 import urllib.request as request
 from re import findall
 
+# Parse version in tuple of numbers
+parse = lambda x: [x[:],tuple(list(map(int, x.split('.'))))]
+
 # function get_info
 #   Creates a file <name> whith the highest version found of a version 
 #   (string) within the given path. If not found, writes '0'  
@@ -11,9 +14,10 @@ from re import findall
 #   cut -  funtion indicating what to keep from the regex pattern
 def get_info(path, regex, name, cut=(lambda x : x)):
     html = str(request.urlopen(path).read())
-    info = cut(max(findall(regex, html)+["0"]))
+    vers = [x[1:] if x[0]=='v' else x for x in findall(regex, html)]
+    latest = max(list(map(parse, vers)), key=lambda x: x[1])[0]
     file = open(name+'.txt', 'w')
-    file.writelines(info)
+    file.writelines(latest)
     file.close()
 
 # Define the FTP URL for downloading and uploading packages
@@ -21,7 +25,7 @@ ftp_path = 'https://oplab9.parqtec.unicamp.br/pub/ppc64el/docker'
 git_path = 'https://github.com/docker/docker-ce/releases/latest'
 
 # find and save the current Github release
-get_info(git_path, 'v\d\d[.]\d\d[.]\d', 'github_version', cut=(lambda x : x[1:]))
+get_info(git_path, 'v\d\d\.\d\d\.\d+', 'github_version', cut=(lambda x : x[1:]))
 
 # find and save the current Docker version on FTP server
-get_info(ftp_path, '\d\d[.]\d\d[.]\d', 'ftp_version')
+get_info(ftp_path, '\d\d\.\d\d\.\d+', 'ftp_version')
