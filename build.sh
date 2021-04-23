9794b0edfcb379a0000863687b1e846face478cf
MatheusCod <41351032+MatheusCod@users.noreply.github.com>
2021-04-23 03:32:42 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -17,10 +17,10 @@ then
     echo "=========> [CLONNING <$git_ver> AND PATCHING] >>>"
     sudo apt-get -y install make
     #sudo printf "deb https://oplab9.parqtec.unicamp.br/pub/repository/debian/ ./\n" >> /etc/apt/sources.list
-    wget https://oplab9.parqtec.unicamp.br/pub/key/openpower-gpgkey-public.asc
-    sudo apt-key add openpower-gpgkey-public.asc
-    sudo apt-get update
-    sudo apt-get -y install docker-ce
+    #wget https://oplab9.parqtec.unicamp.br/pub/key/openpower-gpgkey-public.asc
+    #sudo apt-key add openpower-gpgkey-public.asc
+    #sudo apt-get update
+    #sudo apt-get -y install docker-ce
 
     git clone https://github.com/docker/cli.git
     git clone https://github.com/moby/moby.git
