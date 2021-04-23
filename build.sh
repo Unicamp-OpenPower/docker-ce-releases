bebf19e031503e7352cd080d5f8075b40e69d3f2
MatheusCod <41351032+MatheusCod@users.noreply.github.com>
2021-04-23 03:04:57 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,23 +6,50 @@ url="https://oplab9.parqtec.unicamp.br/pub/ppc64el/docker"
 
 home_dir=$(pwd)
 git_ver=$(cat github_version.txt)
+moby_ver=$(cat moby_version.txt)
 ftp_ver=$(cat ftp_version.txt)
 # del_version=$(cat delete_version.txt)
 
 echo "=========> [CHECKING IF BUILD EXISTS] >>> "
-if [ $git_ver != $ftp_ver ]
+if [ $git_ver != $ftp_ver ] && [ $git_ver == $ftp_ver ]
 then
-
+    
     echo "=========> [CLONNING <$git_ver> AND PATCHING] >>>"
-    git clone https://github.com/docker/docker-ce
-    cd docker-ce && git checkout v$git_ver
-    git config --global user.name "Vinicius Espindola"
-    git config --global user.email "vini.couto.e@gmail.com"
-    python3 ../patch.py
-    git add . && git commit -m "using community containerd versions"
-    sudo make static DOCKER_BUILD_PKGS=static-linux
+    sudo apt -y install make
+    printf "deb https://oplab9.parqtec.unicamp.br/pub/repository/debian/ ./\n" >> /etc/apt/sources.list
+    wget https://oplab9.parqtec.unicamp.br/pub/key/openpower-gpgkey-public.asc
+    sudo apt-key add openpower-gpgkey-public.asc
+    sudo apt-get update
+    sudo apt-get -y install docker-ce
+
+    git clone https://github.com/docker/cli.git
+    git clone https://github.com/moby/moby.git
+    git clone https://github.com/docker/scan-cli-plugin.git
+    git clone https://github.com/docker/docker-ce-packaging.git
+
+    cd cli
+    git checkout v$git_ver
+    cd ..
+
+    cd moby
+    git checkout v$git_ver
+    cd ..
+
+    python3 patch.py
+    mkdir -p docker-ce-packaging/src/github.com/docker/cli
+    mkdir -p docker-ce-packaging/src/github.com//docker/docker
+    mkdir -p docker-ce-packaging/src/github.com/docker/scan-cli-plugin
+
+    sudo cp -r cli/* docker-ce-packaging/src/github.com/docker/cli
+    sudo cp -r moby/* docker-ce-packaging/src/github.com/docker/docker
+    sudo cp -r scan-cli-plugin/* docker-ce-packaging/src/github.com/docker/scan-cli-plugin
 
     echo "=========> [BUILDING <$sys> PACKAGES] >>>"
+    cd $dir
+    sudo make $sys
+    cd && cd $bin_dir
+    ls
+
     cd $home_dir/$dir
     sudo VERSION=$git_ver make $sys
 
@@ -34,27 +61,37 @@ then
     lftp -c "open -u $USER,$PASS $ftp_path/version-$git_ver/$sys; mirror -R ./ ./"
     sudo rm -rf $home_dir/$bin_dir
 
-    if [ ${sys} == "ubuntu-bionic" ]
+    if [ ${sys} == "ubuntu-focal" ]
     then
       echo "=========> [SENDING PACKAGES TO REPOSITORY <$sys>] >>>"
       cd $home_dir
       mkdir upload
       cd upload
-      wget https://oplab9.parqtec.unicamp.br/pub/ppc64el/docker/version-$git_ver/ubuntu-bionic/docker-ce-cli_$git_ver~3-0~ubuntu-bionic_ppc64el.deb
-      wget https://oplab9.parqtec.unicamp.br/pub/ppc64el/docker/version-$git_ver/ubuntu-bionic/docker-ce_$git_ver~3-0~ubuntu-bionic_ppc64el.deb
+      wget https://oplab9.parqtec.unicamp.br/pub/ppc64el/docker/version-$git_ver/ubuntu-bionic/docker-ce-cli_$git_ver~3-0~ubuntu-focal_ppc64el.deb
+      wget https://oplab9.parqtec.unicamp.br/pub/ppc64el/docker/version-$git_ver/ubuntu-bionic/docker-ce_$git_ver~3-0~ubuntu-focal_ppc64el.deb
       lftp -c "open -u $USER,$PASS $ftp_repo1; mirror -R ./ ./"
       cd ..
       rm -rf upload/
     fi
-    if [ ${sys} == "centos" ]
+    if [ ${sys} == "centos-7" ]
     then
       echo "=========> [SENDING PACKAGES TO REPOSITORY <$sys>] >>>"
       cd $home_dir
       mkdir upload
       cd upload
       wget https://oplab9.parqtec.unicamp.br/pub/ppc64el/docker/version-$git_ver/centos/docker-ce-$git_ver-3.el7.ppc64le.rpm
-      wget https://oplab9.parqtec.unicamp.br/pub/ppc64el/docker/version-$git_ver/centos/docker-ce-$git_ver-3.el8.ppc64le.rpm
       wget https://oplab9.parqtec.unicamp.br/pub/ppc64el/docker/version-$git_ver/centos/docker-ce-cli-$git_ver-3.el7.ppc64le.rpm
+      lftp -c "open -u $USER,$PASS $ftp_repo2; mirror -R ./ ./"
+      cd ..
+      rm -rf upload/
+    fi
+    if [ ${sys} == "centos-8" ]
+    then
+      echo "=========> [SENDING PACKAGES TO REPOSITORY <$sys>] >>>"
+      cd $home_dir
+      mkdir upload
+      cd upload
+      wget https://oplab9.parqtec.unicamp.br/pub/ppc64el/docker/version-$git_ver/centos/docker-ce-$git_ver-3.el8.ppc64le.rpm
       wget https://oplab9.parqtec.unicamp.br/pub/ppc64el/docker/version-$git_ver/centos/docker-ce-cli-$git_ver-3.el8.ppc64le.rpm
       lftp -c "open -u $USER,$PASS $ftp_repo2; mirror -R ./ ./"
       cd ..
