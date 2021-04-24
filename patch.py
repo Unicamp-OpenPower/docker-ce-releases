197a71fbf99d0ed09a768f160fc19bb0fb080ada
MatheusCod <41351032+MatheusCod@users.noreply.github.com>
2021-04-24 16:30:47 -0300

    Update gen-rpm-ver version check

--- patch.py
+++ patch.py
@@ -24,4 +24,20 @@ new = re.sub(r'Requires: containerd.io [^\n]*', rpm_ver, data)
 assert data != new, "Nothing was changed in the file."
 open(rpm_path, 'w').write(new)
 
+# Update gen-rpm-ver version check (Make it ignore "-dev")
+newlines = []
+with open('docker-ce-packaging/rpm/gen-rpm-ver', "r") as version_set:
+    i = 0
+    for line in version_set:
+        if i == 45:
+            copy = line.split(' ')
+            copy[3] = '!='
+            newlines.append(' '.join(copy))
+        else:
+            newlines.append(line)
+        i += 1
+        
+with open('docker-ce-packaging/rpm/gen-rpm-ver', "w") as version_set:        
+    version_set.writelines(newlines)
+
 print("DONE Patching")
